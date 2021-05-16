2a45b36bfabf291a003062993eb1a387e105003b
Oscar Dominguez <jokeracm1monrio@gmail.com>
2021-05-15 18:20:04 -0700

    add database tree

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
 #from sqlalchemy import create_engine
 from flask.helpers import url_for
-import psycopg2
+import mysql.connector
 import pandas as pd
 from flask import Flask, render_template, request
 from werkzeug.utils import redirect
-import re
 
 app = Flask(__name__)
-#engine = create_engine('postgresql://postgres:pass123@localhost:5432/postgres')
-#conn = psycopg2.connect("dbname=postgres user=postgres password=pass123")
-conn = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user="postgres",
-    password="pass123")
-#cur = conn.cursor()
+
+conn = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="spk117",
+  database="practica2"
+)
+
 print(conn)
 
-df = pd.read_sql("""SELECT datname FROM pg_database;""", conn)
-df.head()
-print(df.head())
-print(df.columns.values)
+databases = pd.read_sql("SELECT schema_name FROM information_schema.schemata WHERE schema_name not in ('information_schema','mysql','performance_schema','sys');", conn).values
 
-#result = df.to_html(classes='table table-striped')
-# rj = df.to_json()
-#conn.close()
+@app.context_processor
+def utility_processor():
+    def get_tables(db):
+        return pd.read_sql(f"SHOW TABLES FROM {db[0]};", conn).values
+
+    def get_columns(table, db):
+        return pd.read_sql(f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{db[0]}' AND TABLE_NAME = '{table[0]}';", conn).values
+    
+    return dict(get_tables=get_tables, get_columns=get_columns)
 
 db_list = ['postgres', 'db1', 'db2']
 result = ""
@@ -43,11 +45,11 @@ def index():
             result = df.to_html(classes='table table-striped')
         elif 'usa base' in query:
             conn.close()
-            conn = psycopg2.connect(
+            conn = mysql.connector.connect(
                     host="localhost",
-                    database=query.replace('usa base ', '')[:-1],
-                    user="postgres",
-                    password="pass123")
+                    user="root",
+                    password="spk117",
+                    database=query.replace('usa base ', '')[:-1])
         else:
             conn.rollback()
             cur = conn.cursor()
@@ -55,22 +57,8 @@ def index():
             conn.commit()
             cur.close()
 
-        #result = df.to_html(classes='table table-striped')
-        #handle(result)
-    return render_template('index.html', result=result, db_list=db_list)
+    return render_template('index.html', result=result, db_list=databases)
 
-@app.route("/send_query", methods=['POST'])
-def send_query():
-    query = request.form.get('query')
-    print(query)
-    #result = df.to_html(classes='table table-striped')
-    result = df.to_html(classes='table table-striped')
-    return redirect(url_for('index'))
-
-
-def handle(result):
-    result = df.to_html(classes='table table-striped')
-    return result
 
 def reserved_words(query: str):
     if 'lista' in query:
@@ -121,41 +109,3 @@ def reserved_words(query: str):
         query = query.replace('donde', 'where')
 
     return query
-
-equivalents = {
-    'crea base': 'create database',
-    'usa': '\c',
-    'borra base': 'delete database',
-    'crea tabla': 'create table',
-    'borra tabla': 'drop table',
-    'agrega campo': 'alter table',
-    'agrega columna': 'add column',
-    'inserta en': 'insert into',
-    'valores': 'values',
-    'borra': 'delete from',
-    'donde': 'where',
-    'lista': 'select'
-}
-
-'''
-    if 'lista' in query:
-        query = query.replace('lista', 'select')
-        #query = query.replace('de', 'from')
-        query = query.replace('[', '', 1)
-        query = query.replace(']', ' from', 1)
-        #to_move = re.search(r'\[(.*?)\]', query)
-        #query = query.replace('['+to_move+'] ', '')
-        #to_move = to_move[:0] + '(' + to_move[0:] + ')'
-        #query = query.replace('donde', 'donde ' + to_move)
-        if 'donde' in query:
-            to_move = re.search(r'\[(.*?)\]', query)
-            query = query.replace('['+to_move+'] ', '')
-            to_move = to_move[:0] + '(' + to_move[0:] + ')'
-            query = query.replace('donde', 'donde ' + to_move)
-
-            query = query.replace('donde', 'where')
-            if '[y]' in query:
-                query = query.replace('[y]', 'and')
-            if '[o]' in query:
-                query = query.replace('[o]', 'or')
-'''
